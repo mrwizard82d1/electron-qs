7fe6075ae29adc5cb95f41e7610f3030ceb0e897
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-09-20 14:49:01 -0500

    Add application event functions.
    Begin adding functions to respond to application events.
    
    Although I believe I have successfully "ported" the JavaScript
    functions, I do not believe I have created an adequate ClojureScript
    soultion. Specifically, I am not using the required main function to
    help with the initialization.

--- src/electron_qs/core.cljs
+++ src/electron_qs/core.cljs
@@ -3,6 +3,35 @@
 
 (nodejs/enable-util-print!)
 
+(def process (js/require "process"))
+
+;; The module controlling the application life cycle.
+(def app (js/require "app"))
+
+;; A browser window (class).
+(def BrowserWindow (js/require "browser-window"))
+
+;; Report crashes.
+(.start (js/require "crash-reporter"))
+
+;; Retain a reference to the main window so JavaScript does not destroy the instance.
+(def main-window (atom nil))
+
+;; Quit when the user closes all windows.
+(.on app "window-all-closed"
+     ;; On OS X, it is common for applications and their menu bar to remain active until the user explicitly closes
+     ;; the application using Cmd-Q.
+     (fn []
+       (when-not (= "darwin" (.platform process))
+         (.quit app))))
+
+;; When the electron has finished initialization
+(defn do-when-ready []
+  ;; Create the browser window.
+    (swap! main-window BrowserWindow. (clj->js {:width 800 :height 600}))
+  )
+(.on app "ready" do-when-ready)
+
 (defn -main [& args]
   (println "Hello, Electron World!"))
 
